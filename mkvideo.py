54c9b1df8398cb4f6a98861b95592d8dda41d4cd
David <david_why@outlook.com>
2022-04-29 13:52:43 +0800

    feat: Add multithreading support

--- mkvideo.py
+++ mkvideo.py
@@ -1,12 +1,15 @@
+import argparse
 import io
-from math import ceil
+import os
+import shutil
 import struct
 import subprocess
+import tempfile
+import threading
+from math import ceil
+
 import cv2
 from PIL import Image
-import tempfile
-import argparse
-import os
 
 
 def dimensions(s):
@@ -42,6 +45,14 @@ ap.add_argument(
     type=dimensions,
     dest='size',
 )
+ap.add_argument(
+    '-J',
+    '--jobs',
+    help='thread count, default 2',
+    default=2,
+    type=int,
+    dest='jobs',
+)
 args = ap.parse_args()
 
 
@@ -74,62 +85,96 @@ class Mode1:
     def write_frame(self, image, file):
         f = io.BytesIO()
         image.save(f, 'png')
-        d, tf = tempfile.mkstemp('.png')
-        with open(d, 'wb') as of:
+        dirname = tempfile.mkdtemp()
+        # d, tf = tempfile.mkstemp('.png')
+        with open(os.path.join(dirname, 'im.png'), 'wb') as of:
             of.write(f.getvalue())
-        dirname = os.path.dirname(os.path.abspath(tf))
         with open(os.path.join(dirname, 'convimg.yaml'), 'w') as f:
             f.write(
                 '''converts:
  - name: i
    palette: xlibc
    images:
-   - %s
+   - im.png
 outputs:
  - type: bin
    converts:
    - i'''
-                % tf
             )
         proc = subprocess.run(['convimg'], cwd=dirname, capture_output=True)
         if b'[error]' in proc.stdout:
             raise RuntimeError(proc.stdout)
-        os.remove(os.path.join(dirname, 'gfx.txt'))
-        bin = os.path.join(dirname, os.path.splitext(tf)[0] + '.bin')
+        bin = os.path.join(dirname, 'im.bin')
         with open(bin, 'rb') as f:
             f.read(2)
             dat = f.read()
         dat += b'\x00' * (ceil(len(dat) / 512) * 512 - len(dat))
-        os.remove(tf)
-        os.remove(bin)
-        os.remove(os.path.join(dirname, 'convimg.yaml'))
+        shutil.rmtree(dirname)
         file.write(dat)
 
 
 modes = (Mode0, Mode1)
+print_lock = threading.Lock()
+frames = 0
+
+
+def incr_print():
+    with print_lock:
+        global frames
+        frames += 1
+        print(
+            '\rProcessing frame %d/%d' % (frames, fcount), end='', flush=True
+        )
+
+
+class Worker:
+    def __init__(self, idx, start, end, mode):
+        self.idx = idx
+        self.range = range(start, end)
+        self.outfd, self.outfile = tempfile.mkstemp('.bin')
+        self.out = open(self.outfd, 'wb')
+        self.mode = mode
+
+    def run(self):
+        cap = cv2.VideoCapture(args.input)
+        d, f = tempfile.mkstemp('.jpg')
+        os.close(d)
+        for num in self.range:
+            cap.set(cv2.CAP_PROP_POS_MSEC, 1000 / args.fps * num)
+            success, image = cap.read()
+            if not success:
+                break
+            cv2.imwrite(f, image)
+            incr_print()
+            im = Image.open(f).resize(args.size)
+            self.mode.write_frame(im, self.out)
+
+    def write(self, file):
+        self.out.close()
+        with open(self.outfile, 'rb') as f:
+            file.write(f.read())
+        os.remove(self.outfile)
+
 
 cap = cv2.VideoCapture(args.input)
-cap.read()
-d, f = tempfile.mkstemp('.jpg')
-os.close(d)
-num = 0
-with open(args.output, 'wb') as out:
-    mode = modes[args.mode]()
-    mode.write_head(out)
-    fcount = (
-        cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        // cap.get(cv2.CAP_PROP_FPS)
-        * args.fps
-    )
-    while True:
-        cap.set(cv2.CAP_PROP_POS_MSEC, 1000 / args.fps * num)
-        success, image = cap.read()
-        if not success:
-            break
-        print('\rWriting frame %d/%d' % (num, fcount), end='', flush=True)
-        cv2.imwrite(f, image)
-        num += 1
-        im = Image.open(f).resize(args.size)
-        mode.write_frame(im, out)
-os.remove(f)
-print()
+fcount = int(
+    cap.get(cv2.CAP_PROP_FRAME_COUNT) * args.fps // cap.get(cv2.CAP_PROP_FPS)
+)
+jobs = min(args.jobs, fcount)
+each = ceil(fcount / jobs)
+j = []
+wk = []
+mode = modes[args.mode]()
+for i in range(0, fcount, each):
+    w = Worker(i, i, i + each, mode)
+    j.append(threading.Thread(target=w.run))
+    wk.append(w)
+w.range = range(w.range.start, w.range.stop + 50)
+list(map(threading.Thread.start, j))
+list(map(threading.Thread.join, j))
+print('\nCombining output...')
+with open(args.output, 'wb') as f:
+    mode.write_head(f)
+    for w in wk:
+        w.write(f)
+print('Done!')
